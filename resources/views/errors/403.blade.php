923fc4dd2220cec4f7678ee18346e05222457104
Luis Ramirez <me.luisramirez@gmail.com>
2017-08-03 20:53:09 -0600

    Página Error 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="col">
+        <div class="login-register-form d-flex justify-content-md-center align-items-center">
+            <div class="col-md-6">
+                <div class="text-center">
+                    <h1 class="display-1 mb-5">Error 403</h1>
+                    <h3 class="">Lo sentimos, no tienes suficientes permisos para esta area del sistema.</h3>
+                    <p class="text-muted">Para tener los permisos suficientes, puedes contactar a un Administrador.</p>
+                    <a href="/" class="btn btn-secondary">Regresar al Inicio</a>
+                </div>
+            </div>
+        </div>
+</div>
+@endsection
